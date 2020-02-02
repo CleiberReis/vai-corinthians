fbf367e5ed465cd55d33167df9a3b88cc70db81c
CleiberReis <cleiberrodriguesreis@gmail.com>
2020-02-01 23:52:46 -0300

    Ajustando bulma-carousel em 'galeria.html'

--- webpack.config.js
+++ webpack.config.js
@@ -38,29 +38,29 @@ module.exports = {
             },
           },
 
-          {
-            loader: 'image-webpack-loader',
-            options: {
-              mozjpeg: {
-                progressive: true,
-                quality: 65
-              },
-              // optipng.enabled: false will disable optipng
-              optipng: {
-                enabled: false,
-              },
-              pngquant: {
-                quality: [0.65, 0.90],
-                speed: 4
-              },
-              gifsicle: {
-                interlaced: false,
-              },
-              webp: {
-                quality: 75
-              },
-            }
-          },
+          // {
+          //   loader: 'image-webpack-loader',
+          //   options: {
+          //     mozjpeg: {
+          //       progressive: true,
+          //       quality: 95
+          //     },
+          //     // optipng.enabled: false will disable optipng
+          //     optipng: {
+          //       enabled: true,
+          //     },
+          //     pngquant: {
+          //       quality: [0.65, 0.90],
+          //       speed: 4
+          //     },
+          //     gifsicle: {
+          //       interlaced: false,
+          //     },
+          //     webp: {
+          //       quality: 75
+          //     },
+          //   }
+          // },
         ],
       },
 
